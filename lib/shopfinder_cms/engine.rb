c04e50d96336c0ac221bd32fd0cc5e6869a0a4ee
Alexander Penkin <apenkin78@gmail.com>
2013-11-13 23:43:43 +0700

    fix for admin files

--- lib/shopfinder_cms/engine.rb
+++ lib/shopfinder_cms/engine.rb
@@ -4,8 +4,7 @@ module ShopfinderCms
 
     initializer :shopfinder_cms do
       #ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '/lib/shopfinder_cms/admin']
-      ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/shopfinder_cms/admin']
-      #ActiveAdmin.application.load_paths += Dir['/Users/aleksandr/apps/shopfinder_cms/shopfinder_cms' + '/lib/shopfinder_cms/admin']
+      ActiveAdmin.application.load_paths.unshift (File.dirname(__FILE__) + '/admin')
     end
   end
 end
