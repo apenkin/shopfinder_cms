8517aeec3ba662cd4105c29ebf5541411a95fab2
Alexander <apenkin78@gmail.com>
2013-11-13 06:11:04 -0800

    Update engine.rb

--- lib/shopfinder_cms/engine.rb
+++ lib/shopfinder_cms/engine.rb
@@ -3,8 +3,8 @@ module ShopfinderCms
     isolate_namespace ShopfinderCms
 
     initializer :shopfinder_cms do
-      #ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '/lib/shopfinder_cms/admin']
-      ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/shopfinder_cms/admin']
+      ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '/lib/shopfinder_cms/admin']
+      #ActiveAdmin.application.load_paths.unshift Dir[File.dirname(__FILE__) + '/shopfinder_cms/admin']
       #ActiveAdmin.application.load_paths += Dir['/Users/aleksandr/apps/shopfinder_cms/shopfinder_cms' + '/lib/shopfinder_cms/admin']
     end
   end
