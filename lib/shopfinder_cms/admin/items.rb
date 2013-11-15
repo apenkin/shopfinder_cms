a8d8c27d9160f14f166054dd065b9c65270d49cb
Alexander Penkin <apenkin78@gmail.com>
2013-11-16 02:25:52 +0700

    fixed items form

--- lib/shopfinder_cms/admin/items.rb
+++ lib/shopfinder_cms/admin/items.rb
@@ -24,15 +24,10 @@ if defined?(ActiveAdmin)
     end
     form do |f|
       f.inputs "Admin Details" do
-        f.input :document_type, as: :hidden, input_html: {value: 'Category'}
-        f.input :document_id, :as => :hidden, :input_html => {:id => 'hidden_category_id'}
-        f.input :document_name, :label => 'Category', :input_html => {:class => 'find_category'}
         f.input :title
         f.input :text, :input_html => {:rows => 6}
         f.input :short_text
         f.input :image
-        f.input :image2
-        f.input :author
         f.input :published_at, :as => :datepicker
         f.input :meta_title
         f.input :meta_description
